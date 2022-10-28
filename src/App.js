1392865bba6626dfa5329284dfb8134620bbf0ac
hidayat83 <sinung83@gmail.com>
2022-10-29 01:42:40 +0700

    testing to heroku

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br/>Oke tambahan dikit
         </p>
         <a
           className="App-link"
